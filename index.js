c8f4f3953d63cae4bf27dfdb96904625d0b6fa0d
chilechile <>
2021-05-17 12:00:41 +0000

    finished work

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 //Create a variable called myObject and assign 
 //an object to it.
-
+var myObject = "car";
 
 
 
 //Add two properties of firstName and lastName
 //using dot notation
+let obj = {
+  firstName: "Alexis
+  lastName: "Montgomery"
+};
+let obj = obj.Alexis + Montgomery 
+
+console.log(firstName + lastName)
 
 
 
@@ -13,23 +20,37 @@
 //Add two properties of hobbies and age 
 //using bracket notation
 //hobbies must be an array of at least 4 strings
+var myHobbies = ['sleeping', 'singing', 'dancing',]
+age:77
+};
+console.log(hobbies['name'+ 'age'])
 
 
 
 
 //Print the myObject variable
-
+car 
 
 
 
 //Use a for in loop to print all of the keys of the object
 
-
+var i;
+for (i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
 
 
 ///Use a for in loop to print all of the values of the object
+}
+var numbers = [2, 4, 6, 8, 12];
 
-
+var lengthInFeet = "";
+var x;
+for (x in numbers) {
+  txt += numbers[x] + "<br>";
+}
+document.getElementById("demo").innerHTML = lengthInFeet;
 
 
 
@@ -39,3 +60,4 @@ create an object that is about a specific car
 Include: year, make, model, color and an array of features that you want your car to have
 
 */
+var car = {type:'Lexus' model:"X6" color:"Black"}
